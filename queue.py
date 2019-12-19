05316b8cea274795b940c0a24e239ea2291b6ab5
Diana Badas <diana.badas@gmail.com>
2019-12-19 17:12:39 +0000

    queue

--- queue.py
+++ queue.py
@@ -1,1 +1,50 @@
+# queue is an abstract data type
+# Basic operations: enqueue() and dequeue(), peek()
+# FIFO structure, first in first out
+# Can be implemented with dynamic arrays as well as with linked lists.
+# Important when implementing BFS algorithms for graphs
+# Applications:
+    # When a resource is shared with several consumers, like CPU scheduling
+   # When data is tranferred asynchronously between two processes, like IO buffers
+   # Operational research apps or stochastic models
 
+#FIFO
+
+class Queue:
+    
+    def __init__(self):
+        self.queue = []
+
+    def isEmpty(self):
+        return self.queue == []
+    
+    def enqueue(self, data):
+        self.queue.append(data)
+
+    def dequeue(self):
+        data = self.queue[0]
+        del self.queue[0]
+        return data
+
+    def peek(self):
+        data = self.queue[0]
+        return data
+
+    def sizeQueue(self):
+        return len(self.queue)
+
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+queue.enqueue(5)
+
+print(queue.sizeQueue())
+
+print("Popped ", queue.dequeue())
+print("Popped ", queue.dequeue())
+print(queue.sizeQueue())
+print("Peeked: ", queue.peek())
+print(queue.sizeQueue())
